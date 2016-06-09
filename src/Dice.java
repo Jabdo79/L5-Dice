5e6a594357bb42d959955c43779bf68c69be205f
Jonathan Abdo <jabdo79@gmail.com>
2016-06-09 15:57:39 -0400

    first commit

---
+++ src/Dice.java
@@ -0,0 +1,42 @@
+/*Jonathan Abdo 6.9.2016
+ * create an application that simulates dice rolling
+ */
+import java.util.Random;
+import java.util.Scanner;
+
+public class Dice {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to the Grand Circus Casino!\n");
+		
+		Scanner sc = new Scanner(System.in);
+		int sides = InputCheck.getInt(sc, "How many sides do you want the dice to have? ");
+		System.out.println(sides+" it is!");
+		//call method to roll dice
+		rollDice(sc, sides);
+		
+		sc.close();
+	}
+	
+	public static void rollDice(Scanner sc, int sides){
+		//count rolls made
+		int counter=1;
+		boolean cont = true;
+		Random rnd = new Random();
+		int die1=0, die2=0;
+		while(cont){
+			if(InputCheck.cont(sc, "\nRoll the dice? (y/n): ")){
+				System.out.println("\nRoll "+counter);
+				//rolls die 1 using Math.random, +1 accounts for 'lost side'
+				die1 = (int)(Math.random()*sides)+1;
+				System.out.println("Die 1: "+die1);
+				//rolls die 2 using Random.nextInt, +1 accounts for 'lost side'
+				die2 = rnd.nextInt(sides)+1;
+				System.out.println("Die 2: "+die2);
+			}else
+				cont=false;
+		}
+	}
+
+}
